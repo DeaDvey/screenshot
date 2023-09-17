a3de0781fe3ac2d062658a61331d36657782810e
danmax <danmax71@protomail.com>
2023-09-17 13:29:56 -0400

    replaced /home/deadvey with /home/meme_man in screenshot.sh

--- screenshot.sh
+++ screenshot.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 time=$(date +"%d,%m,%Y[]%H:%M:%S")
-#maim -s /home/deadvey/Screenshots/$time.png
+#maim -s $HOME/Screenshots/$time.png
 copyq &
 #maim -s --format png /dev/stdout | copyq copy image/png - | -u
-maim -s --format png /home/deadvey/Screenshots/$time.png | -u
-cat "/home/deadvey/Screenshots/$time.png" | copyq copy image/png -
+maim -s --format png $HOME/Screenshots/$time.png | -u
+cat "$HOME/$time.png" | copyq copy image/png -
