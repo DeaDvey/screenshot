6234f7e857a9e3f25161b3cd1a6f76d5ec85dad7
danmax <danmax71@protomail.com>
2023-09-17 13:40:05 -0400

    fixed a typo

--- screenshot.sh
+++ screenshot.sh
@@ -4,4 +4,4 @@ time=$(date +"%d,%m,%Y[]%H:%M:%S")
 copyq &
 #maim -s --format png /dev/stdout | copyq copy image/png - | -u
 maim -s --format png $HOME/Screenshots/$time.png | -u
-cat "$HOME/$time.png" | copyq copy image/png -
+cat "$HOME/Screenshots/$time.png" | copyq copy image/png -
